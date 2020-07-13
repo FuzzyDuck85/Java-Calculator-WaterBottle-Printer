7d9e8ca7402be06b2acf9591b4ecc3b03a0b0207
Donald Cameron <drc85@live.co.uk>
2020-07-13 18:42:33 +0100

    printer homework completed

--- Printer/src/main/java/Printer.java
+++ Printer/src/main/java/Printer.java
@@ -1,2 +1,26 @@
 public class Printer {
+
+    private int paper;
+    private int toner;
+
+    public Printer (int paper, int toner){
+        this.paper = paper;
+        this.toner = toner;
+    }
+
+    public int getPaper() {
+        return paper;
+    }
+
+    public int getToner() {
+        return toner;
+    }
+
+    public void print(int noOfPages, int requieredCopies) {
+        int totalPages = noOfPages * requieredCopies;
+        if(this.paper >= totalPages){
+            this.paper -= totalPages;
+            this.toner -= totalPages;
+        }
+    }
 }
--- Printer/src/test/java/PrinterTest.java
+++ Printer/src/test/java/PrinterTest.java
@@ -1,2 +1,51 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashSet;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrinterTest {
+
+    Printer printer;
+
+    @Before
+    public void before(){
+        printer = new Printer(50, 45);
+    }
+
+    @Test
+    public void hasPaper(){
+        assertEquals(50, printer.getPaper());
+    }
+
+    @Test
+    public void hasToner(){
+        assertEquals(45, printer.getToner());
+    }
+
+    @Test
+    public void canPrintWhenEnoughPaper() {
+        printer.print(5, 2);
+        assertEquals(40, printer.getPaper());
+    }
+
+    @Test
+    public void willNotPrintIfPaperSupplyTooLow(){
+        printer.print(10, 6);
+        assertEquals(50, printer.getPaper());
+        assertEquals(45, printer.getToner());
+    }
+
+    @Test
+    public void tonerLevelReducesWhenPrinting(){
+        printer.print(1,1);
+        assertEquals(44, printer.getToner());
+    }
+
+    @Test
+    public void canPrintWhenTonerLevelSufficient(){
+        printer.print(5, 2);
+        assertEquals(35, printer.getToner());
+    }
 }
